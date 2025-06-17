1b81e40e8d6741db2cb74b0ae5d1a0701e620aad
Jesús Domínguez Ramírez <jesus.dominguez.ramirez855@gmail.com>
2025-06-17 11:07:07 -0600

    Creación de algunas funciones

--- firebase-realtime-db/src/App.js
+++ firebase-realtime-db/src/App.js
@@ -1,8 +1,24 @@
-function App() {
-  let random = process.env.REACT_APP_TEST 
+import { useEffect, useState } from "react";
+import database from './utils/firebaseConfig.js'
+import { ref, set, push, get, onValue } from "firebase/database";
+function App() { 
+  const [text,setText] = useState("")
+
+  function save(){
+    messagesRef = ref(database,'test');
+    set(messagesRef)
+  }
+
+  useEffect(()=>{
+    const suscribe = onValue(ref(database,'test'),(snapshot)=>{
+      console.log(snapshot)
+    })
+    return suscribe()
+  },[])
   return (
     <div>
-      <p>React App Variable: {random} / App Variable: {process.env.TESTING}</p>
+      <input type="text" value={text} onChange={(e)=>setText(e.target.value)}></input>
+      <button onClick={(e)=>console.log(text)}></button>
     </div>
   );
 }
